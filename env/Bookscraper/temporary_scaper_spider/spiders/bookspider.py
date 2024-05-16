651decc46b6e871b0ef4ce8bcf5faf213bb917a9
mtauha <111292199+mtauha@users.noreply.github.com>
2024-05-17 02:51:39 +0500

    feat: Add bookspider.py to scrape book data from website
    This commit adds the bookspider.py file, which contains a Scrapy spider to crawl the website "https://books.toscrape.com/" and scrape book data. The spider follows links to individual book pages and extracts information such as title, genre, description, product type, price, tax, availability, number of reviews, and star rating.

---
+++ env/Bookscraper/temporary_scaper_spider/spiders/bookspider.py
@@ -0,0 +1,44 @@
+import scrapy
+
+
+class BookspiderSpider(scrapy.Spider):
+    name = "bookspider"
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com"]
+
+    def parse(self, response):
+        books = response.css('article.product_pod')
+
+        for book in books:
+            book_url = book.css('h3 a ::attr(href)').get()
+            if book_url is not None:
+                if 'catalogue' not in book_url:
+                    next_url = str(self.start_urls[0]) + f'/catalogue/{book_url}'
+                else:
+                    next_url = str(self.start_urls[0]) + f'/{book_url}'
+                yield response.follow(next_url, callback=self.parse_book_page)
+            
+        next_page = response.css('li.next a ::attr(href)').get()
+        if next_page is not None:
+            if 'catalogue' not in next_page:
+                    next_url = str(self.start_urls[0]) + f'/catalogue/{next_page}'
+            else:
+                    next_url = str(self.start_urls[0]) + f'/{next_page}'
+        yield response.follow(next_url, callback=self.parse)
+    
+    def parse_book_page(self, response):
+        table = response.xpath('//table[@class="table table-striped"]')
+        rows = table.xpath('//tr')
+
+        yield{
+            "title" : response.xpath('//div[@class="col-sm-6 product_main"]/h1[1]/text()').get(),
+            "genre" : response.xpath('.//ul[@class="breadcrumb"]/li[@class="active"]/preceding-sibling::li[1]/a/text()').get(),
+            "description": response.xpath('//article[@class="product_page"]/p/text()').get(),
+            "product_type": rows[1].xpath('.//td/text()').get(),
+            "price_excl_tax": rows[2].xpath('.//td/text()').get(),
+            "price_incl_tax": rows[3].xpath('.//td/text()').get(),
+            "tax": rows[4].xpath('.//td/text()').get(),
+            "availability": rows[5].xpath('.//td/text()').get(),
+            "num_reviews": rows[6].xpath('.//td/text()').get(),
+            "stars": response.xpath('//*[@id="content_inner"]/article/div[1]/div[2]/p[3]/@class').get(),
+        }
