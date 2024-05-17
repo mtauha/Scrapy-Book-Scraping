8b69bb45bdf10a020ef3396a2bb1a308b1bf3acc
mtauha <111292199+mtauha@users.noreply.github.com>
2024-05-18 02:13:20 +0500

    chore: Refactor items.py
    For better code organization and readability

--- env/Bookscraper/temporary_scaper_spider/items.py
+++ env/Bookscraper/temporary_scaper_spider/items.py
@@ -5,8 +5,17 @@
 
 import scrapy
 
+def serialize_price(value):
+    return f'£ {str(value)}' 
 
-class TemporaryScaperSpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class BookItem:
+    title = scrapy.Field()
+    genre = scrapy.Field()
+    product_type = scrapy.Field()
+    price_excl_tax = scrapy.Field()     # scrapy.Field(serializer=serialize_price)
+    price_incl_tax = scrapy.Field()     # scrapy.Field(serializer=serialize_price) 
+    tax =  scrapy.Field()               # scrapy.Field(serializer=serialize_price) 
+    availability = scrapy.Field()
+    num_reviews = scrapy.Field()
+    stars = scrapy.Field()
+    description = scrapy.Field()
