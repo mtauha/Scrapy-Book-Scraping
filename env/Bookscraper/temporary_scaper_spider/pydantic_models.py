0de1249721067139664c943109a22bfc105268cc
mtauha <111292199+mtauha@users.noreply.github.com>
2024-06-17 00:51:11 +0500

    feat: Add Book model and update pydantic models

--- env/Bookscraper/temporary_scaper_spider/pydantic_models.py
+++ env/Bookscraper/temporary_scaper_spider/pydantic_models.py
@@ -15,3 +15,21 @@ class BookModel(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class Book(BaseModel):
+    id:int
+    url:str
+    title: str
+    product_type: str
+    price_excl_tax: float
+    price_incl_tax: float
+    tax: float
+    availability: int
+    num_reviews: int
+    stars: int
+    category: str
+    description: str
+
+    class Config:
+        from_attributes = True
